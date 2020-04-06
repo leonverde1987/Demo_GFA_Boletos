6829f71caa4e44d8c3b0e97eff74874473ca8705
davidchaveza <david.chavez.avila@gmail.com>
2020-04-06 12:49:21 -0500

    Se agrega Reporte con html y pdf

--- src/test/java/generic/genericGrid.java
+++ src/test/java/generic/genericGrid.java
@@ -24,8 +24,7 @@ import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.remote.RemoteWebDriver; 
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
